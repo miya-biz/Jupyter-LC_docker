1e75255c32c0c06e003d81dedbc6f1211faa881a
Taku Miyamoto <40813148+miyamoto83@users.noreply.github.com>
2024-06-12 16:18:51 +0900

    wait for docker and log with timeout

--- ui-tests/tests/docker.spec.ts
+++ ui-tests/tests/docker.spec.ts
@@ -17,6 +17,7 @@ function delay(ms: number) {
   return new Promise( resolve => setTimeout(resolve, ms) );
 }
 
+test.setTimeout(90000);
 test('should emit an activation console message', async ({ page }) => {
   const logs: string[] = [];
 
@@ -25,37 +26,81 @@ test('should emit an activation console message', async ({ page }) => {
   });
 
   // docker上のJupyter Labが起動するまで待機
-  await delay(10000);
-  // doxker上のJupyter Labに遷移
-  await page.goto('');
-  // 待機しないとエラーになる
-  await delay(5000);
+  const timeout = 30000;
+  let startDate = Date.now();
+  while(true) {
+    await delay(1000);
+    // タイムアウトのチェック
+    if(Date.now() - startDate >= timeout) {
+      console.log("wait for docker: timeout!");
+      break;
+    }
+
+    try {
+      let response = await page.goto('');
+      if(response && response.status() == 200) {
+        console.log("wait for docker: finished.");
+        break;
+      }
+    } catch (e) {
+      console.log(`network error: ${e.message}`);
+    }
+    console.log("wait for docker: continue...");
+  }
+
+  // ログの待機
+  const log_lc_index = 'JupyterLab extension lc_index is activated!';
+  const log_lc_multi_outputs = 'JupyterLab extension lc_multi_outputs is activated!';
+  const log_lc_notebook_diff = 'JupyterLab extension lc_notebook_diff is activated!';
+  const log_lc_run_through = 'JupyterLab extension lc_run_through is activated!';
+  const log_nblineage = 'JupyterLab extension nblineage is activated!';
+  startDate = Date.now();
+  while(true) {
+    await delay(100);
+    // タイムアウトのチェック
+    if(Date.now() - startDate >= timeout) {
+      console.log("wait for docker: timeout!");
+      break;
+    }
+
+    if(
+      logs.filter(s => s === log_lc_index).length == 1 &&
+      logs.filter(s => s === log_lc_multi_outputs).length == 1 &&
+      logs.filter(s => s === log_lc_notebook_diff).length == 1 &&
+      logs.filter(s => s === log_lc_run_through).length == 1 &&
+      logs.filter(s => s === log_nblineage).length == 1
+    ) {
+      console.log("wait for docker: finished.");
+      break;
+    }
+    console.log("wait for log: continue...")
+  }
   
   // lc_index
   expect(
-    logs.filter(s => s === 'JupyterLab extension lc_index is activated!')
+    logs.filter(s => s === log_lc_index)
   ).toHaveLength(1);
   
   // lc_multi_outputs
   // console.debug
   expect(
-    logs.filter(s => s === 'JupyterLab extension lc_multi_outputs is activated!')
+    logs.filter(s => s === log_lc_multi_outputs)
   ).toHaveLength(1);
 
   // lc_notebook_diff
   expect(
-    logs.filter(s => s === 'JupyterLab extension lc_notebook_diff is activated!')
+    logs.filter(s => s === log_lc_notebook_diff)
   ).toHaveLength(1);
   
   // lc_run_through
   // console.debug
   expect(
-    logs.filter(s => s === 'JupyterLab extension lc_run_through is activated!')
+    logs.filter(s => s === log_lc_run_through)
   ).toHaveLength(1);
 
   // nblineage
   expect(
-    logs.filter(s => s === 'JupyterLab extension nblineage is activated!')
+    logs.filter(s => s === log_nblineage)
   ).toHaveLength(1);
 
   // nbsearch(disabled)
